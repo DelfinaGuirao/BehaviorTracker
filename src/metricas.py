377016d08adb74773b191402c99e420a9b437fbb
DelfinaGuirao <dguirao@udesa.edu.ar>
2026-03-26 15:43:08 -0300

    src/

---
+++ src/metricas.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 15:42:46 2026
+
+@author: delfi
+"""
+
---
+++ src/procesamiento_datos.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 15:42:21 2026
+
+@author: delfi
+"""
+

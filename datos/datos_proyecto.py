fc07db4d7210d60d3d1837d47b958e391847dc65
DelfinaGuirao <dguirao@udesa.edu.ar>
2026-03-26 15:39:21 -0300

    ji

---
+++ datos/datos_proyecto.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 15:38:45 2026
+
+@author: delfi
+"""
+
+#hola3
---
+++ main.py/programa_principal.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 15:37:44 2026
+
+@author: delfi
+"""
+
+#hola2

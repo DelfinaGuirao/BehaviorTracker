92c5a7a0995a99c6fb87f5aedd396a328825a906
Leilamena <lmena@udesa.edu.ar>
2026-03-26 16:00:14 -0300

    Update cargar_datos.py
    funcion cargar datos

--- src/cargar_datos.py
+++ src/cargar_datos.py
@@ -1,8 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 15:36:44 2026
+def parsear_linea (linea):
+    linea_strip=linea.strip("\n")
+    linea_split=linea_strip.split(";")
+    registro={"id_participante": int(linea_split[0]), "fecha":linea_split[1], "app":linea_split[2], "cantidad_uso":int(linea_split[3]), "tiempo_uso":float(linea_split[4])}
+    return registro
+def cargar_datos (ruta): 
+    datos=[]
+    archivo=open(ruta,"r")
+    for linea in archivo:
+        if linea.strip() != "":
+            registro = parsear_linea(linea)
+            datos.append(registro)
+    archivo.close()
 
-@author: delfi
-"""
-
-#hola
+    return datos

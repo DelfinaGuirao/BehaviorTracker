f147f3d9709356901308c7b62f8877ff8f94de8a
josefinahermida <jhermida@udesa.edu.ar>
2026-04-01 15:35:20 -0300

    Update validacion_datos.py

--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -1,8 +1,33 @@
-# -*- coding: utf-8 -*-
+    
 """
-Created on Thu Mar 26 15:40:36 2026
+def validar_registro(registro):
 
-@author: delfi
-"""
+    Que hace la funcion: 
+        Verifica los tipo de datos y que los valores no esten vacios ni sean negativos
 
-#hola 4
+    Parameteros:
+        registro(dicc): diccionario con los datos de un registro     
+
+    Retornos:
+        bool: True si el registro es valido y False si el registro no es valido
+
+    """
+
+
+def validar_registro(registro):
+    if "id_participante" not in registro or "fecha" not in registro or "app" not in registro or "cantidad_uso" not in registro or"tiempo_uso" not in registro:
+        return False
+    
+    if registro["fecha"] == "":
+        return ("la fecha ingresada es incorrecta")
+    if registro["app"] == "":
+        return ("el nombre de la aplicacion es incorrecto")
+    if registro["id_participante"] <= 0:
+        return ("el id del participante es incorrecto")
+    if registro["cantidad_uso"] <0:
+        return ("el valor ingresado en cantidad de uso es incorrecto")
+    if registro["tiempo_uso"] <0:
+        return ("el valor ingresado en tiempo de uso es incorrecto")
+    
+    return True
+ 
